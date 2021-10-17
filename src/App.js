0eb4ae17455c51f1c587a261ad6144ae2006f140
Sean-Mabli <smdrone1@gmail.com>
2021-10-16 23:14:15 -0400

    change navbar to react

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
+import react from 'react';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <header className="App">
-		  <div class="nav">
-		  	<a class="tablink" href='index.html'>Home</a>
-		  	<b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b> 
-		  	<a class="tablink" href='ElectricalEngineering.html'>Electrical Engineering</a>
-		  	<b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>
-		  	<a class="tablink" href='ArtificialIntelligence.html'>Artificial Intelligence</a>
-		  	<b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b>
-		  	<a class="tablink" href='About.html'>About</a>
-		  </div>
-    </header>
+    <div className="App">
+			<Navbar />
+    </div>
   );
 }
 
---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import react from "react";
+import './Button.css'
+
+const STYLES = [
+  'btn--primary',
+  'btn--outline'
+]
+
+const SIZES = [
+  'btn--medium',
+  'btn--large'
+]
+
+export const Button = ({
+  children,
+  type,
+  onClick,
+  buttonStyle,
+  buttonSize
+}) => {
+
+}
