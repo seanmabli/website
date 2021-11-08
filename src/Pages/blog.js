85f4649df1e353da5957be62eb47189c255ec594
SeanMabli <smdrone1@gmail.com>
2021-11-08 17:25:01 -0500

    work on rectangle and blog content

--- src/Pages/blog.js
+++ src/Pages/blog.js
@@ -1,10 +1,23 @@
 import './blog.css'
+import Rectangle from 'react-rectangle';
+import blogcontent from './blogcontent';
+import { Link } from 'react-router-dom';
 
 function Blog() {
-  return (
-    <div>
-      <h1 className={'blog'}>example text, i want to keep this long so that I can see how it looks</h1>
-    </div>
+  return(
+    <nav className='blog'>
+      <u1 className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}> 
+        {blogcontent.map((item, index) => {
+          return (
+            <li key={index}>
+              <Link to={item.url} className={'nav-links'}>
+                {item.title}
+              </Link>
+            </li>
+          ) 
+        })} 
+      </u1>
+    </nav>
   )
 }
 
---
+++ src/Pages/blogcontent.js
@@ -0,0 +1,16 @@
+const blogcontent = [
+  {
+    title: 'post-one',
+    url: '/post-one',
+  },
+  {
+    title: 'post-two',
+    url: '/post-two',
+  },
+  {
+    title: 'post-three',
+    url: '/post-three',
+  }
+]
+
+export default blogcontent
