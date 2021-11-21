b3ab0a60d387e1c95aa340c6335fceb4b63cbae1
SeanMabli <smdrone1@gmail.com>
2021-11-21 15:18:53 -0500

    firebase inport

--- src/index.js
+++ src/index.js
@@ -6,12 +6,28 @@ import Post from './post';
 import './index.css';
 
 ReactDOM.render(
-    <Router>
-      <Switch>
-        <Route exact path="/" component={blog} />
+  <Router>
+    <Switch>
+      <Route exact path="/" component={blog} />
 
-        <Route path="/:id" component={Post} />
-      </Switch>
-    </Router>,
+      <Route path="/:id" component={Post} />
+    </Switch>
+  </Router>,
 
-  document.getElementById('root'));
+document.getElementById('root'));
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFLpjJ7-5yDGXm0sIaXKZ1RSh7X4BHdq0",
+  authDomain: "seanmabli-website.firebaseapp.com",
+  projectId: "seanmabli-website",
+  storageBucket: "seanmabli-website.appspot.com",
+  messagingSenderId: "202273441435",
+  appId: "1:202273441435:web:d72e44884fa14a28b0683e",
+  measurementId: "G-H6M65EG29S"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
