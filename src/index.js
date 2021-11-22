254c75b616b4f26225f4a3956eac6f637e90e136
SeanMabli <smdrone1@gmail.com>
2021-11-22 16:45:37 -0500

    newest upload to firebase

--- src/index.js
+++ src/index.js
@@ -15,19 +15,3 @@ ReactDOM.render(
   </Router>,
 
 document.getElementById('root'));
-
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCFLpjJ7-5yDGXm0sIaXKZ1RSh7X4BHdq0",
-  authDomain: "seanmabli-website.firebaseapp.com",
-  projectId: "seanmabli-website",
-  storageBucket: "seanmabli-website.appspot.com",
-  messagingSenderId: "202273441435",
-  appId: "1:202273441435:web:d72e44884fa14a28b0683e",
-  measurementId: "G-H6M65EG29S"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
