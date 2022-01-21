c528e9df47ece4aafb83e8c535425cab2378d8b2
sean mabli <70709738+seanmabli@users.noreply.github.com>
2022-01-21 20:18:27 +0000

    add test post

--- src/blog.js
+++ src/blog.js
@@ -1,8 +1,15 @@
 import { Link } from 'react-router-dom';
 import content from './content';
 import './index.css';
+import { doc, getDoc } from "firebase/firestore";
 
 function Blog() {
+
+  const docRef = doc(db, "cities", "SF");
+  const docSnap = getDoc(docRef);
+
+  console.log("Document data:", docSnap.data());
+  
   return(
     <div className="site">
       <div className="about">
