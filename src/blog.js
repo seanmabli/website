cb6bb8b0e9a19c91caec53c8714f35b3733e3b3c
seanmabli <smdrone1@gmail.com>
2022-01-23 12:24:47 -0500

    add basic firestore test

--- src/blog.js
+++ src/blog.js
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
+import {db} from './firebase'
+import { collection, getDocs } from "firebase/firestore";
 import content from './content';
 import './index.css';
 
 function Blog() {
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "blog")
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+    };
+    getUsers()
+  }, [])
+
   return(
     <div className="site">
       <div className="about">
         <p className="p box">sean mabli</p>
         <p className='h2'>I am the creator of aiinpy, an open source python package used to create machine learning models.</p>
+	<p className="h2">{users.map((user) => {return(
+		<div>
+			{" "}
+			<p className="h2">{user.title}</p>
+		</div>);
+	})}</p>
         <div className="box">
           <a href="https://aiinpy.org" className="h1 lighter link">aiinpy</a> 
           <a href="https://github.com/seanmabli" className="h1 lighter link">&nbsp;github</a> 
--- src/index.js
+++ src/index.js
@@ -5,27 +5,10 @@ import Blog from './blog';
 import Post from './post';
 import './index.css';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDi1AVuX6kiInM9bNMdq0xiWgU77rC_5sk",
-  authDomain: "sean-mabli.firebaseapp.com",
-  projectId: "sean-mabli",
-  storageBucket: "sean-mabli.appspot.com",
-  messagingSenderId: "662981595137",
-  appId: "1:662981595137:web:7e3cd0cfffe02f35d07000",
-  measurementId: "G-GQ489QS53Z"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 ReactDOM.render(
   <Router>
     <Switch>
       <Route exact path="/" component={Blog} />
-
       <Route path="/:id" component={Post} />
     </Switch>
   </Router>,
