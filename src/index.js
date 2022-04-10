7bfc18bfc5f8b118263f4551c2d4a8a2526e6bb8
sean mabli <smdrone1@gmail.com>
2022-04-10 02:55:07 +0000

    remove all firebase dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Blog from './blog';
 import Post from './post';
 import './index.css';
