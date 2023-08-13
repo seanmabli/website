7eee2807dab05235e7ef3ffe612f330682ec76b8
Sean Mabli <70709738+seanmabli@users.noreply.github.com>
2023-08-13 00:48:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ root.render(
         <a href="https://github.com/seanmabli">Github</a>
       </div>
       <h2>The Ultrasonicists <h3>(World Robot Olympiad Team)</h3></h2>
+      <p> - 2019 USA Nationals (Robomission Junior Category): 10th place</p>
+      <p> - 2022 USA Nationals (Robomission Senior Category): 1st place</p>
+      <p> - 2022 WRO Internationals (Robomission Senior Category): 26th place</p>
+      <p> - 2023 USA Nationals (Robomission Senior Category): 1st place</p>
       <div className="linkContainer">
         <a href="https://www.youtube.com/@ultrasonicists">
            YouTube
@@ -20,7 +24,12 @@ root.render(
       </div>
       <div className="linkContainer">
         <a href="https://github.com/seanmabli/wro">
-         Github
+         Github (2021, 2022)
+        </a>
+      </div>
+      <div className="linkContainer">
+        <a href="https://github.com/seanmabli/wro2023">
+         Github (2023)
         </a>
       </div>
       <h2>aiinpy</h2>
