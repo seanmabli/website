d5cdf42a577a1ba3501eb67e5c3cffd27a0b9ec2
Sean Mabli <70709738+seanmabli@users.noreply.github.com>
2024-01-13 20:29:42 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,24 +5,28 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className="site">
     <div>
-      <h1>Sean Mabli <h3>(seanmabli4@gmail.com)</h3></h1>
+      <h1>Sean Mabli <h3>(seanmabli4@gmail.com)</h3>
+      </h1>
       <div className="linkContainer">
         <a href="https://www.youtube.com/@seanmabli">YouTube</a>
       </div>
       <div className="linkContainer">
         <a href="https://github.com/seanmabli">Github</a>
       </div>
-      <h2>The Ultrasonicists <h3>(World Robot Olympiad Team)</h3></h2>
+      <h2>The Ultrasonicists <h3>(World Robot Olympiad Team)</h3>
+      </h2>
       <div className="linkContainer">
-        <a href="https://www.youtube.com/@ultrasonicists">
-           YouTube
-        </a>
+        <a href="https://www.youtube.com/@ultrasonicists"> YouTube </a>
       </div>
       <div className="linkContainer">
-        <p><a href="https://github.com/seanmabli/wro">Github</a> (2021, 2022)</p>
+        <p>
+          <a href="https://github.com/seanmabli/wro">Github</a> (2021, 2022)
+        </p>
       </div>
       <div className="linkContainer">
-        <p><a href="https://github.com/seanmabli/wro2023">Github</a> (2023)</p>
+        <p>
+          <a href="https://github.com/seanmabli/wro2023">Github</a> (2023)
+        </p>
       </div>
       <h2>aiinpy</h2>
       <div className="linkContainer">
@@ -33,6 +37,13 @@ root.render(
       </div>
       <div className="linkContainer">
         <a href="https://github.com/seanmabli/aiinpy">Github</a>
+      </div>
+      <h2>Dataset Colab</h2>
+      <div className="linkContainer">
+        <a href="https://datasetcolab.com">Dataset Colab</a>
+      </div>
+      <div className="linkContainer">
+        <a href="https://github.com/team4169/datasetcolab">Github</a>
       </div>
     </div>{" "}
   </div>
