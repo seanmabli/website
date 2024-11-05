9e7bc9eccc8975bca4ebfb8b1690bf9bee36562f
seanmabli <seanmabli4@gmail.com>
2024-11-04 19:46:20 -0500

    small updates

--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ function Link({ href, children }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <>
-    <a 
-      href={href} 
-      target="_blank" 
-      rel="noopener noreferrer" 
-      style={{ textDecoration: 'none' }}
-      onMouseEnter={() => setIsHovered(true)} 
+    <div
+      onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
+      style={{ display: 'grid', gridTemplateColumns: '230px 1fr' }}
     >
-      <span style={{ textDecoration: 'underline' }}>{children}</span>
-      {isHovered && <span> ({href.replace(/^https?:\/\//, '').replace(/^www\./, '')})</span>}
-    </a>
-    <br />
-  </>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+      >{children}
+      </a>
+      {isHovered && <p>({href.replace(/^https?:\/\//, '').replace(/^www\./, '')})</p>}
+    </div>
   );
 }
 
@@ -27,8 +26,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className="site">
     <div>
-      <h1>Sean Mabli <h3>(sean@synodic.ai)</h3>
-      </h1>
+      <div style={{ marginBottom: "20px" }}>
+        <h1 style={{ margin: 0 }}>Sean Mabli</h1>
+        <p>seanmabli4@gmail.com</p>
+        <p>978.460.5363</p>
+      </div>
       <Link href="https://github.com/seanmabli">Github</Link>
       <Link href="https://x.com/seanmabli">X</Link>
       <Link href="https://www.youtube.com/@seanmabli">YouTube</Link>
@@ -37,6 +39,7 @@ root.render(
       <Link href="https://www.youtube.com/watch?v=ZZaLB54iTHM">Indian Diaspora Interview</Link>
       <Link href="https://github.com/synodicai">Github</Link>
       <Link href="https://x.com/synodicai">X</Link>
+      <Link href="https://youtube.com/@synodic_ai">YouTube</Link>
       <h2>The Ultrasonicists <h3>(World Robot Olympiad Team)</h3></h2>
       <Link href="https://www.youtube.com/@ultrasonicists">YouTube</Link>
       <Link href="https://github.com/seanmabli/wro2023">Github (2023)</Link>
